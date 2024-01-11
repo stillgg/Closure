89092cb332c7cd9c3b61a08eeae718461992d753
Egor <still2095@gmail.com>
2024-01-12 00:49:07 +0300

    solve lab

--- Exercises/1-seq.js
+++ Exercises/1-seq.js
@@ -1,5 +1,30 @@
-'use strict';
+"use strict";
 
-const seq = (f) => (g) => (x) => 0;
+const seq = (f) => (g) => (x) => {
+  if (typeof x === "function") {
+    return seq;
+  }
+  if (typeof x === "number") {
+    return f(g(x));
+  }
+};
+
+console.log(seq((x) => x + 7)((x) => x * 2)((x) => x * 3)(5));
+
+// ```js
+// seq(x => x + 7)
+//    (x => x * 2)(5)
+
+// // Результат: 17
+// ``````js
+// seq(x => x * 2)
+//    (x => x + 7)(5)
+
+// // Результат: 24
+// ``````js
+// console.log(seq((x) => x + 1)((x) => x * 2)((x) => x / 3)((x) => x - 4)(7));
+
+// // Результат: 3
+// ```;
 
 module.exports = { seq };
--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,17 @@
-'use strict';
+"use strict";
 
-const array = () => null;
+const array = () => {
+  const arr = [];
+
+  const f = (index) => {
+    if (typeof index === "number") return arr[index];
+    return f;
+  };
+
+  f.push = (el) => arr.push(el);
+  f.pop = () => arr.pop();
+
+  return f;
+};
 
 module.exports = { array };
